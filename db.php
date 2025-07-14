9c80a8958dd07e859417543e7a93049477b624db
sarthak99 <sarthakmahinge99@gmail.com>
2025-07-15 00:58:30 +0530

    Updated db.php with public network MySQL credentials

--- db.php
+++ db.php
@@ -1,13 +1,7 @@
 <?php
-$conn = new mysqli(
-  "yamabiko.proxy.rlwy.net",   // Host
-  "root",                      // User
-  "vazoWsJrBqTkBEQIYJdkXhzRAoJyGdkF",   // Password (from Railway)
-  "railway",                   // Database name
-  57099                        // Port
-);
+$conn = new mysqli("localhost:3307", "root", "", "kc03db");
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
