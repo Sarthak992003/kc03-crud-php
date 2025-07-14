053aa119f411cf07132bb2a01e6588820c20353e
sarthak99 <sarthakmahinge99@gmail.com>
2025-07-14 16:39:46 +0530

    Updated db.php with Railway MySQL credentials

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "crud_app");
+$conn = new mysqli(
+  "mysql.railway.internal", // ← Host
+  "root",                              // ← Username
+  "vazoWsJrBqTkBEQIYJdkXhzRAoJyGdkF",          // ← Password from Railway
+  "railway",                           // ← Database name
+  3306                                 // ← Port
+);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
